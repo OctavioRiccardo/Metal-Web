97349a594d7992e559db916ab2f56db44ae73b7e
RobLucho <lucho.aguilar.hl.38@gmail.com>
2023-06-10 17:51:57 -0300

    Cambios al formulario

--- MetalWeb/Metal.js
+++ MetalWeb/Metal.js
@@ -1,5 +1,24 @@
 /*----------------------------------------Funcion de Paginacion------------------------------------------------------- */
 
 
+/*----------------------------------------Funcion de Validacion Pagina de formulario------------------------------------------------------- */
+ function validacion() {
+    var btn = document.getElementById("boton");
+    btn.addEventListener('click', versionPropia());
+  }
+  
+  function versionPropia() {
+    var forms = document.querySelectorAll('.needs-validation');
+    forms.forEach(function(form) {
+      if (form.checkValidity()) {
+        form.classList.add('was-validated');
+        form.innerHTML = "";
+      } else {
+        form.classList.add('was-validated');
+      }
+    });
+  }
+  
+/*---------------------------------------------------------------------------------------------------------------------------------------- */  
 
 /*-----------------------------------------------Peticiones HTTP-------------------------------------------------------*/
