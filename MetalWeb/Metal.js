73cba797b17ed346986b65d4156abc77bda4c832
RobLucho <lucho.aguilar.hl.38@gmail.com>
2023-06-11 17:12:24 -0300

    cambiosForm

--- MetalWeb/Metal.js
+++ MetalWeb/Metal.js
@@ -14,70 +14,26 @@ function versionPropia() {
       form.classList.add('was-validated');
       form.innerHTML = "";
       form.innerHTML = `
-        <form class="row g-3 needs-validation" novalidate>
-        <div class="col-md-4">
-          <label for="validationCustom01" class="form-label">First name</label>
-          <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
+      <form class="row g-3 needs-validation" style="display: none;" novalidate>
+      <div class="col-md-8" style="border: 1px solid black;">
+          <label for="validaNombre" class="form-label">Nombre:</label>
+      </div>
+      <div class="col-md-4">
+          <input type="text" class="form-control" id="validaNombre" required>
           <div class="valid-feedback">
-            Looks good!
+              Looks good!
           </div>
-        </div>
-        <div class="col-md-4">
-          <label for="validationCustom02" class="form-label">Last name</label>
-          <input type="text" class="form-control" id="validationCustom02" value="Otto" required>
+      </div>
+      <div class="col-md-8" style="border: 1px solid black;">
+          <label for="validaApellido" class="form-label">Apellido:</label>
+      </div>
+      <div class="col-md-4">
+          <input type="text" class="form-control" id="validaApellido" required>
           <div class="valid-feedback">
-            Looks good!
+              Looks good!
           </div>
-        </div>
-        <div class="col-md-4">
-          <label for="validationCustomUsername" class="form-label">Username</label>
-          <div class="input-group has-validation">
-            <span class="input-group-text" id="inputGroupPrepend">@</span>
-            <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
-            <div class="invalid-feedback">
-              Please choose a username.
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <label for="validationCustom03" class="form-label">City</label>
-          <input type="text" class="form-control" id="validationCustom03" required>
-          <div class="invalid-feedback">
-            Please provide a valid city.
-          </div>
-        </div>
-        <div class="col-md-3">
-          <label for="validationCustom04" class="form-label">State</label>
-          <select class="form-select" id="validationCustom04" required>
-            <option selected disabled value="">Choose...</option>
-            <option>...</option>
-          </select>
-          <div class="invalid-feedback">
-            Please select a valid state.
-          </div>
-        </div>
-        <div class="col-md-3">
-          <label for="validationCustom05" class="form-label">Zip</label>
-          <input type="text" class="form-control" id="validationCustom05" required>
-          <div class="invalid-feedback">
-            Please provide a valid zip.
-          </div>
-        </div>
-        <div class="col-12">
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-            <label class="form-check-label" for="invalidCheck">
-              Agree to terms and conditions
-            </label>
-            <div class="invalid-feedback">
-              You must agree before submitting.
-            </div>
-          </div>
-        </div>
-        <div class="col-12">
-          <button class="btn btn-primary" type="submit">Submit form</button>
-        </div>
-      </form>
+      </div>
+  </form>
         `;
     } else {
       form.classList.add('was-validated');
